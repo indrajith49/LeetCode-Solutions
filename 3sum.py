3da81e30ce9966a580669ca9225f61b4c2c22094
Indra不是indro <indrajithbarua194@gmail.com>
2026-06-25 14:53:33 +0800

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -169,3 +169,46 @@ res.append([a, nums[l], nums[r]])	Add to results	Store the triplet
 l += 1; r -= 1	Move pointers	Continue searching
 Skip duplicates	Avoid duplicates	Prevent duplicate triplets
 return res	Return results	All triplets found"""
+
+
+ANOTHER SOLUTION DONE BY ME:
+class Solution:
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+
+            if nums[i]>0:
+                break
+
+            l, r = i+1, len(nums) - 1
+            while l<r:
+                threesome = nums[i] + nums[l] + nums[r]
+
+                if threesome<0:
+                    l+=1
+                elif threesome>0:
+                    r-=1
+
+                if threesome == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l+=1
+                    r-=1
+
+                    while l<r and nums[l] == nums[l-1]:
+                        l+=1
+
+                    while l<r and nums[r]== nums[r+1]:
+                        r-=1
+
+
+        return res
+
+
+
+
+
