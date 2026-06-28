ab0c002bca2532a013c2d7d3dced9137d6a74236
Indra不是indro <indrajithbarua194@gmail.com>
2026-06-28 16:09:02 +0800

    Create 33. Search in Rotated Sorted Array.py

---
+++ 33. Search in Rotated Sorted Array.py
@@ -0,0 +1,25 @@
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        l, r  = 0, n-1
+
+
+        while l<=r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[l] <= nums[mid]:  # Left half is sorted
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:  # Right half is sorted
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
+sol = Solution()
+print(sol.search([4,5,6,7,0,1,2], 1))
