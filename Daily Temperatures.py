0aafbfef9772817872dcf1710d049422a82bd97d
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-07-10 15:35:55 +0800

    Create Daily Temperatures.py

---
+++ Daily Temperatures.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+
+        n = len(temperatures)
+        result = [0] * n
+        stack = []
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                previous_index = stack.pop()
+                result[previous_index] = i-previous_index
+            stack.append(i)
+        return result
+
+sol = Solution()
+print(sol.dailyTemperatures([73,74,75,71,69,72,76,73]))
+
