007f52465f1a7be1c49013e7f9fd31854bc767e5
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-07-12 17:55:08 +0800

    347. Top K Frequent Elements.py

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -8,3 +8,26 @@ class Solution:
             result.append(num)
             
         return result
+
+
+
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+import heapq
+from collections import Counter
+
+class Solution:
+    def top_k_frequent(self, nums, k):
+        counter = Counter(nums)
+        result = []
+
+        for num, freq in counter.items():
+            heapq.heappush(result, (num, freq))
+
+            if len(result)>k:
+                heapq.heappop(result )
+
+        return [num for freq, num in result]
+
+sol = Solution()
+print(sol.top_k_frequent([1,1,1,2,2,3], 2))
