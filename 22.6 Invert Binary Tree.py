13365d9ed1e8b413c5c9d158020043b171ea5b73
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-07-18 15:19:26 +0800

    Update 22.6 Invert Binary Tree.py

--- 22.6 Invert Binary Tree.py
+++ 22.6 Invert Binary Tree.py
@@ -15,3 +15,20 @@ class Solution:
         self.invertTree(root.right)
 
         return root
+--------------------------------------------WITHOUT RECURSION--------------------------------------------
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return None
+        queue = [root]
+
+        while queue:
+            node = queue.pop(0)
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        return root
