4a188a5c8f594a970039de09f68224720604fbaa
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-07-19 18:19:14 +0800

    Update 101. Symmetric Tree.py

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -22,3 +22,29 @@ class Solution:
         return (left.val == right.val and
             self.isMirror(left.left, right.right) and
             self.isMirror(left.right, right.left))
+
+----------------------------------------ALTERNATIVE--------------------------------------------------------
+
+class Solution:
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.isMirror(root.left, root.right)
+
+
+    def isMirror(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+
+        if left.val== right.val and self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left):
+
+            return True
+        return False
+
+        """return  (left.val == right.val and
+                self.isMirror(left.left, right.right) and
+                self.isMirror(left.right, right.left))"""
+
+
