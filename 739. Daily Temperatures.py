9bcd9136aaeeb69ef755560d02c8435e6a1b12a9
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-07-27 20:35:22 +0800

    739. Daily Temperatures.py

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -1,8 +1,28 @@
 ---------------------------------------------------CORE IDEA--------------------------------------------
+We store indices in a stack. 
+As long as the stack is not empty and the current temperature is greater than the temperature at the top index of the stack, 
+we pop that index, subtract it from the current index to get the number of days waited, 
+and store it in the answer array. After resolving all cooler days, we push the current index onto the stack. 
+Finally, we return the answer array.
+-----------------------------------------------------BRUTE FORCE-----------------------------------------------------
 
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        answer = [0]*n
+        for i in range(n):
+            for j in range(i+1, n):
+                if temperatures[j]>temperatures[i]:
+                    answer[i] = j-i
+                    break
+        return answer
 
+sol = Solution()
+print(sol.dailyTemperatures([73,74,75,71,69,72,76,73]))
 
+------------------------------------------------------------------------------------------------------------------------
 
+------------------------------------------------------STACK & QUEUE------------------------------------------------------
 class Solution:
     def dailyTemperatures(self, temperatures):
 
