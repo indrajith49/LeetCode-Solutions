93393986964e52cb4c8cae153b81e6f57f36407e
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-07-30 14:34:11 +0800

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,22 @@
+
+---------------------------------------------------------------------NOOB-------------------------------------------------------------------------------
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j] 
+
+---------------------------------------------------------------------MORE OPTIMIZED-------------------------------------------------------------------------------
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
+---------------------------------------------------------------------CORE IDEA FOR MORE OPTIMIZED-------------------------------------------------------------------------------
