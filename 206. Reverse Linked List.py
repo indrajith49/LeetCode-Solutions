f033bef6706556f9a0bc8d388df57cbc3bfcf691
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-08-08 20:52:53 +0800

    Create 206. Reverse Linked List.py

---
+++ 206. Reverse Linked List.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None          # Previous node (starts as None)
+        curr = head          # Current node (starts at head)
+
+        while curr:          # While there are still nodes to process
+            temp = curr.next  # Save the next node (so we don't lose it)
+            curr.next = prev  # Reverse the arrow: point curr to prev
+            prev = curr       # Move prev forward
+            curr = temp       # Move curr forward
+
+        return prev          # prev is the new head
