c851619486029b9e3227f3771780b2b4bf233902
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-08-20 20:38:50 +0800

    Add files via upload

---
+++ 438. Find All Anagrams in a String.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res = []
+        len_p = len(p)
+        len_s = len(s)
+
+        count_p = [0] * 26
+        window_count = [0] * 26
+
+        if len_s < len_p:
+            return res
+        for ch in p:
+            count_p[ord(ch) - ord("a")] += 1
+
+
+        for i in range(len_p):
+            window_count[ord(s[i]) - ord("a")] += 1
+
+        return window_count
+
+
+sol = Solution()
+print(sol.findAnagrams("cbaebabacd", p = "abc"))
