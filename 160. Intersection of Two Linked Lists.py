4617a1f621cb6b7b43e4f6eb56ab7dfc5d05b62b
Indra是刘浩 <indrajithbarua194@gmail.com>
2026-08-20 21:46:01 +0800

    Add files via upload

---
+++ 160. Intersection of Two Linked Lists.py
@@ -0,0 +1,32 @@
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        stackA = []
+        stackB = []
+
+
+        current = headA
+        while current:
+            stackA.append(current)
+            current = current.next
+
+        current = headB
+        while current:
+            stackB.append(current)
+            current = current.next
+
+
+        prev = None
+        while stackA and stackB:
+            nodeA = stackA.pop()
+            nodeB = stackB.pop()
+            if nodeA == nodeB:
+                prev = nodeB
+            else:
+                break
+        return prev
